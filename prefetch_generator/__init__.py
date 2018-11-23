61e26b2cbc25b73137057105a3eef330784d3ff1
justheuristic <justheuristic@gmail.com>
2018-11-24 02:13:32 +0300

    fix issue by  mahnerak@

--- prefetch_generator/__init__.py
+++ prefetch_generator/__init__.py
@@ -96,10 +96,10 @@ class BackgroundGenerator(threading.Thread):
 
 #decorator
 class background:
-    def __init__(self,max_prefetch=1):
+    def __init__(self, max_prefetch=1):
         self.max_prefetch = max_prefetch
-    def __call__(self,gen):
+    def __call__(self, gen):
         def bg_generator(*args,**kwargs):
-            return BackgroundGenerator(gen(*args,**kwargs))
+            return BackgroundGenerator(gen(*args,**kwargs), max_prefetch=self.max_prefetch)
         return bg_generator
 

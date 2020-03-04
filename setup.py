9e72d17da8f9dc30b24148266b8dbf7e770bb430
David Völgyes <david.volgyes@ieee.org>
2020-03-04 01:56:59 +0100

    Add python 3.6-3.8 support, version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import prefetch_generator
 # loading README
 long_description = prefetch_generator.__doc__
 
-version_string = '1.0.0'
+version_string = '1.0.2'
 
 setup(
     name="prefetch_generator",
@@ -37,9 +37,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7 ',
-        'Programming Language :: Python :: 3.4 ',
-        'Programming Language :: Python :: 3.5 ',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
 
     ],
 
